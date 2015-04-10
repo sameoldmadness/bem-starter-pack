0d7b7ee97fe41acbbb5eb9cde4313d4659122775
Roman Paradeev <grumpy@yandex-team.ru>
2015-04-10 18:59:23 +0300

    readme init

--- src/js/trivial-bem.js
+++ src/js/trivial-bem.js
@@ -1,5 +1,5 @@
 function BEM(fn) {
-    modules.require(['bh', 'i-bem__dom'], function(BEMHTML, BEMDOM) {
+    modules.require(['bh', 'i-bem__dom', 'jquery'], function(BEMHTML, BEMDOM, $) {
         BEM.HTML = BEMHTML;
         BEM.DOM = BEMDOM;
         fn();
