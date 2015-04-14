74c0e6535d74a5b0b692201f2bc976d1cbbc1579
Roman Paradeev <grumpy@yandex-team.ru>
2015-04-15 00:29:16 +0500

    archive

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,46 @@ var runSequence = require('run-sequence');
 
 var pkg = require('./package.json');
 
+gulp.task('archive:create_archive_dir', function () {
+    fs.mkdirSync(path.resolve('archive'), '0755');
+});
+
+gulp.task('archive:zip', function (done) {
+
+    var archiveName = path.resolve('archive', pkg.name + '_v' + pkg.version + '.zip');
+    var archiver = require('archiver')('zip');
+    var files = require('glob').sync('**/*.*', {
+        cwd : 'dist',
+        dot : true // include hidden files
+    });
+    var output = fs.createWriteStream(archiveName);
+
+    archiver.on('error', function (error) {
+        done();
+        throw error;
+    });
+
+    output.on('close', done);
+
+    files.forEach(function (file) {
+
+        var filePath = path.resolve('dist', file);
+
+        // `archiver.bulk` does not maintain the file
+        // permissions, so we need to add files individually
+        archiver.append(fs.createReadStream(filePath), {
+            name : file,
+            mode : fs.statSync(filePath)
+        });
+
+    });
+
+    archiver.pipe(output);
+    archiver.finalize();
+});
+
 gulp.task('clean', function (done) {
-    require('del')(['dist', 'bower_components'], done);
+    require('del')(['archive', 'dist', 'bower_components'], done);
 });
 
 gulp.task('copy', [
@@ -54,6 +92,14 @@ gulp.task('lint:js', function () {
 
 gulp.task('install:bower', function () {
     return plugins.bower();
+});
+
+gulp.task('archive', function (done) {
+    runSequence(
+        'build',
+        'archive:create_archive_dir',
+        'archive:zip',
+    done);
 });
 
 gulp.task('build', function (done) {
